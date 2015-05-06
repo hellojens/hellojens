12e043feb4fd62df1da23753c3f0c5d50118002b
Jens Krogshede <jens@retelly.com>
2015-05-06 17:14:36 +0200

    About page done

--- wp-content/themes/aorta-theme/template-about.php
+++ wp-content/themes/aorta-theme/template-about.php
@@ -6,8 +6,12 @@
  			<section class="small-10 small-centered columns">
 
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-					<h2><?php echo get_the_content(); ?></h2>
+					<div class="small-12 columns">
+						<h2><?php echo get_the_content(); ?></h2>
+					</div>
+					<div class="small-12 medium-6 left columns">
+						<p><?php the_field("about_extra_info"); ?></p>
+					</div>
 
       	<?php endwhile; endif;?>    
 
@@ -15,12 +19,37 @@
 		</div>			
 	</div>
 
-	<div class="content-section">
+	<div class="about-social">
 		<div class="row"> 
-			<section class="small-10 medium-11 small-centered columns main-grid">
-				<div class="row">
 
-				</div>
+				<?php if( get_field('about_facebook') ): ?>
+					<section class="small-12 small-centered columns social-block">
+						<div class="row">
+							<div class="small-10 small-centered columns">
+								<a class="social-link" target="_blank" href="<?php the_field("about_facebook"); ?>">Facebook ></a>
+							</div>
+						</div>
+					</section>
+				<?php endif; ?>
+				<?php if( get_field('about_twitter') ): ?>
+					<section class="small-12 small-centered columns social-block">
+						<div class="row">
+							<div class="small-10 small-centered columns">
+								<a class="social-link" target="_blank" href="<?php the_field("about_facebook"); ?>">Twitter ></a>
+							</div>
+						</div>
+					</section>
+				<?php endif; ?>
+				<?php if( get_field('about_instagram') ): ?>
+					<section class="small-12 small-centered columns social-block">
+						<div class="row">
+							<div class="small-10 small-centered columns">
+								<a class="social-link" target="_blank" href="<?php the_field("about_instagram"); ?>">Instagram ></a>
+							</div>
+						</div>
+					</section>
+				<?php endif; ?>
+
 			</section>
 		</div>
 	</div>
