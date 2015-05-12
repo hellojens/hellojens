acb8d877d381881b9270f6edebe1c0d8684a59de
Jens Krogshede <jens@retelly.com>
2015-05-12 12:42:05 +0200

    hellojens.com partical fix

--- wp-content/themes/hellojens/js/particles.js
+++ wp-content/themes/hellojens/js/particles.js
@@ -1,61 +1,110 @@
 particlesJS('particles-js', {
-  particles: {
-    color: '#D84B4B',
-    color_random: false,
-    shape: 'triangle', // "circle", "edge" or "triangle"
-    opacity: {
-      opacity: .6,
-      anim: {
-        enable: false,
-        speed: 2.5,
-        opacity_min: 0.3,
-        sync: false
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 1000
       }
     },
-    size: 4.5,
-    size_random: true,
-    nb: 50,
-    line_linked: {
-      enable_auto: true,
-      distance: 200,
-      color: '#aeaeae',
-      opacity: 1,
-      width: 1,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
+    "color": {
+      "value": "#D84B4B"
     },
-    anim: {
-      enable: true,
-      speed: 1
-    }
-  },
-  interactivity: {
-    enable: true,
-    mouse: {
-      distance: 250
-    },
-    detect_on: 'window', // "canvas" or "window"
-    mode: 'grab', // "grab" of false
-    line_linked: {
-      opacity: .7
-    },
-    events: {
-      onclick: {
-        enable: true,
-        mode: 'push', // "push" or "remove"
-        nb: 3
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#D84B4B"
       },
-      onresize: {
-        enable: true,
-        mode: 'bounce', // "out" or "bounce"
-        density_auto: true,
-        density_area: 1000 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+      "polygon": {
+        "nb_sides": 3
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 4,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 120,
+      "color": "#aeaeae",
+      "opacity": 0.8,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 600
       }
     }
   },
-  /* Retina Display Support */
-  retina_detect: true
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 150,
+        "size": 0,
+        "duration": 2,
+        "opacity": 0,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 300,
+        "duration": 0.2
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 });
