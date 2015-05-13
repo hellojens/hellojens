8135d9b60ab386ab01d71a6108ab69f0bac4728d
Jens Krogshede <jens@retelly.com>
2015-05-13 16:41:24 +0200

    Aorta theme ready for review

--- wp-content/themes/aorta-theme/template-home.php
+++ wp-content/themes/aorta-theme/template-home.php
@@ -80,20 +80,6 @@
 			<section class="small-12 large-11 small-centered text-center columns main-grid">
 				<div class="row">
 					<?php include(locate_template('content-people-all.php')); ?>
-					
-				  <div class="small-6 medium-4 large-3 left columns">
-				    <article id="post-<?php the_ID(); ?>" <?php post_class(array("class" => "people-post")); ?>>
-				      <div class="round-wrapper">
-				      <?php $featuredimage = wp_get_attachment_image_src( get_post_thumbnail_id(), post ); ?>
-				        <a href="<?php the_permalink(); ?>">
-				          <img src="<?php echo $featuredimage[0]; ?>">
-				        </a>
-				      </span>
-				      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				      <?php if (function_exists('the_subheading')) { the_subheading('<p>', '</p>'); } ?>
-				    </article>
-				  </div>
-
 				</div>
 			</section>
 		</div>
